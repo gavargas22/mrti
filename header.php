35150370a12256fd63c96c6229c928012924ea81
Guillermo Vargas <gavargas22@gmail.com>
2011-12-14 16:48:06 -0700

    Finished making the parts the main webpage.

---
+++ header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Materials Research and Technology Institute | The University of Texas at El Paso</title>
+<link href="<?php bloginfo('stylesheet_directory'); ?>/style.css" rel="stylesheet" type="text/css" />
+<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+<script type="text/javascript" src="<?php bloginfo('stylesheet_directory'); ?>/js/slides.min.jquery.js"></script>
+<script type="text/javascript" src="<?php bloginfo('stylesheet_directory'); ?>/js/init.js"></script>
+</head>
+
+<body>	
+	
+<!-- Facebook Code for the Like Page of Facebook. -->
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_GB/all.js#xfbml=1";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
---
+++ index.php
@@ -0,0 +1,63 @@
+<?php get_header(); ?>
+	
+	<div id="main-wrapper">
+		<div id="header">
+			<div id="mrti-logo"><a href="#">MRTI Home</a></div>
+			<div id="footer-logo-utep"><a href="http://www.utep.edu">UTEP Home</a></div>
+			<div id="lab-title"></div>
+			<div id="header-images"></div>
+			<div id="menu-wrapper">
+				<ul>
+					<li><a href="#">Home</a></li>
+					<li><a href="#">News</a></li>
+					<li><a href="#">Research</a></li>
+					<li><a href="#">Projects</a></li>
+					<li><a href="#">Faculty</a></li>
+				</ul>
+			</div>
+		</div>
+		<div id="slideshow-news">
+			<div id="slides">
+				<div class="slides_container">
+					<div><a href="#"><img src="<?php bloginfo('stylesheet_directory'); ?>/image/slides/slide-1.jpg" alt="Slide 1"></a></div>
+					<div><img src="<?php bloginfo('stylesheet_directory'); ?>/image/slides/slide-2.jpg" alt="Slide 2"></div>
+					<div><img src="<?php bloginfo('stylesheet_directory'); ?>/image/slides/slide-3.jpg" alt="Slide 3"></div>
+					<div><img src="<?php bloginfo('stylesheet_directory'); ?>/image/slides/slide-4.jpg" alt="Slide 4"></div>
+				</div>
+			</div>
+			<div id="shadow-holder"></div>
+			<div id="storyboard">
+				<div id= "storyboard-title">Recent Stories</div>
+				<div id="storyboard-stories">
+					<?php query_posts( 'cat=3' ); ?>
+					<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+					<div class="storyboard-story" id="storyboard-story-<?php the_ID(); ?>">
+						<div class="story-title" id="story-title-<?php the_ID(); ?>"><a href="#"><?php the_title(); ?></a></div>
+						<div class="story-date" id="story-date-<?php the_ID(); ?>"><?php the_time('F jS, Y') ?></div>
+					</div>
+					<?php endwhile; else: ?>
+					<p>Sorry, no posts matched your criteria.</p>
+					<?php endif; ?>
+				</div>
+			</div>
+		</div>
+		<div id="body-page">
+			<div id="body-info-wrapper">
+				<?php query_posts( 'cat=4' ); ?>
+				<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+				<div id="body-info-title"><?php the_title(); ?></div>
+				<div id="body-info-content"><?php the_content(); ?></div>
+				<?php endwhile; else: ?>
+				<p>Sorry, no posts matched your criteria.</p>
+				<?php endif; ?>
+			</div>
+			<div id="facebook-like-wrapper"><div class="fb-like-box" data-href="https://www.facebook.com/pages/Materials-Research-and-Technology-Institute/199458250141759" data-width="230" data-height="350" data-show-faces="true" data-border-color="#eee" data-stream="true" data-header="false"></div></div>
+		</div>
+		<div id="footer">
+			<div id="footer-logo"></div>
+			<div id="footer-menu"><a href="#">Home</a> | <a href="#">News</a> | <a href="#"> Research</a> | <a href="#">Projects</a> | <a href="#">Faculty</a></div>
+			<div id="copyright-notice">Copyright © 2011-2012 The University of Texas at El Paso. Designed and Developed by <a href="mailto:gavargas@slackentie.com">Guillermo Vargas</a> @ SlackenTie Group</div>
+		</div>
+	</div>
+	
+<?php get_footer(); ?>
