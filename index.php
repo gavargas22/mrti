9fea882e451da7f9539c77775ab6765634013e39
Guillermo Vargas <gavargas22@gmail.com>
2012-07-12 15:46:18 -0600

    Update master

--- index.php
+++ index.php
@@ -51,7 +51,7 @@
 		</div>
 		<div id="footer">
 			<div id="footer-logo"></div>
-			<div id="footer-menu"><a href="#">Home</a> | <a href="#">News</a> | <a href="#"> Research</a> | <a href="#">Projects</a> | <a href="#">Faculty</a> | <a href="wp-admin">Site Admin</a> | <?php wp_loginout(); ?></div>
+			<div id="footer-menu"><a href="#">Home</a> | <a href="#">News</a> | <a href="#"> Research</a> | <a href="#">Projects</a> | <a href="#">Faculty</a> | <a href="./wp-admin">Site Admin</a> | <?php wp_loginout(); ?></div>
 			<div id="copyright-notice">Copyright © 2011-2012 The University of Texas at El Paso. Designed and Developed by <a href="mailto:gavargas@slackentie.com">Guillermo Vargas</a> @ SlackenTie Group</div>
 		</div>
 	</div>
